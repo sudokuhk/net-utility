e61e2abd4b0d55cef8d1411b460cd7b6db346bf4
sudoku.huang <sudoku.huang@gmail.com>
2016-12-05 12:31:38 +0800

    1. fix http boundary parser.

--- uhttp/uboundaryparser.cpp
+++ uhttp/uboundaryparser.cpp
@@ -14,14 +14,6 @@ uboundaryparser::~uboundaryparser()
 {
 }
 
-/*
-First boundary: ------WebKitFormBoundaryzpnYCFS44g1YQgyR\r\n
-    Encapsulated multipart part:  (image/jpeg)
-        Content-Disposition: form-data; name="userfile"; filename="\345\257\270\347\205\247_\351\273\204\345\235\244.jpg"\r\n
-        Content-Type: image/jpeg\r\n\r\n
-        JPEG File Interchange Format
-            Marker: Start of Image (0xffd8)
-*/
 bool uboundaryparser::parse(const std::string & content, const char * boundary,
     std::vector<uboundaryinfo> & output)
 {
@@ -40,13 +32,25 @@ bool uboundaryparser::parse(const std::string & content, const char * boundary,
     std::string::size_type idx     = 0;
     const char * data = NULL;
     std::string::size_type len     = 0;
+    bool first                     = true;
 
     uboundaryinfo info;
         
     while ((pos = content.find(boundaryin, begin)) != std::string::npos) {
 
+        if (first) {
+            first = false;
+        } else {
+            info.data   = content.data() + begin;
+            info.len    = pos - begin - 2;
+            
+            if (info.len > 0) {
+                output.push_back(info);
+            } 
+        }
+        
         begin = pos + boundaryin.size();
-        if (content.size() > begin + 2) {
+        if (content.size() >= begin + 2) {
             if (content[begin] == '-' && content[begin + 1] == '-') {
                 suc =  true;
                 break;
@@ -88,7 +92,7 @@ bool uboundaryparser::parse(const std::string & content, const char * boundary,
         
         data    = content.data() + begin;
         len     = pos - begin;
-        while (*data == ' ' || *data == '"') {
+        while (len > 0 && (*data == ' ' || *data == '"')) {
             data ++;
             len --;
         }
@@ -108,7 +112,7 @@ bool uboundaryparser::parse(const std::string & content, const char * boundary,
         
         data    = content.data() + begin;
         len     = pos - begin;
-        while (*data == ' ' || *data == '"') {
+        while (len > 0 && (*data == ' ' || *data == '"')) {
             data ++;
             len --;
         }
@@ -128,27 +132,16 @@ bool uboundaryparser::parse(const std::string & content, const char * boundary,
         
         data    = content.data() + begin;
         len     = pos - begin;
-        while (*data == ' ') {
+        while (len > 0 && *data == ' ') {
             data ++;
             len --;
         }
         std::string(data, len).swap(info.type);
 
-        begin = pos + sizeof("\r\n");
-        if (content.size() > begin + 2 && content[begin] == '\r' 
+        begin = pos + 2; //sizeof("\r\n");  // skip \r\n
+        if (content.size() >= begin + 2 && content[begin] == '\r' 
             && content[begin + 1] == '\n') {
-            begin += 2;
-
-            pos = content.find("\r\n", begin);
-            if (pos == std::string::npos) {
-                break;
-            }
-            info.data   = content.data() + begin;
-            info.len    = pos - begin;
-
-            output.push_back(info);
-            
-            begin = pos + 2;
+            begin += 2;                     // skip \r\n
         } else {
             break;
         }
